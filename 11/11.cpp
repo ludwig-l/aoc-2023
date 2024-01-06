098346f77366ae2d4f1145c1701f0c4e17d215f0
Ludwig Lohmer <ludwig-lohmer@web.de>
2024-01-06 18:37:16 +0100

    Add creation of galaxy pairs and calculation of shortest distances

--- 11/11.cpp
+++ 11/11.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <utility>
+#include <cmath>
+#include <numeric>
 
 
-const std::string INPUT_FILE = "test_input.txt";
-//const std::string INPUT_FILE = "puzzle_input.txt";
+//const std::string INPUT_FILE = "test_input.txt";
+const std::string INPUT_FILE = "puzzle_input.txt";
 const char CHAR_GALAXY = '#';
 const char CHAR_EMPTY_SPACE = '.';
 
@@ -62,4 +65,44 @@ int main(int argc, char** argv)
     expand_universe(universe);
 
     std::cout << "Finished\n";
+
+    /*
+    next part thoughts:
+    *   assign a number to each galaxy (assigned in raster-scan order) and a row/column coordinate pair
+    *   for each galaxy pair: find the shortest path connecting the two galaxies
+
+    */
+   unsigned int n_rows = universe.size();
+   unsigned int n_cols = universe[0].size(); // all row entries have the same length
+   std::vector<std::pair<unsigned int, unsigned int>> galaxy_locations = {};
+   for (unsigned int row = 0; row < n_rows; row++) {
+       for (unsigned int col = 0; col < n_cols; col++) {
+           if (universe[row][col] == CHAR_GALAXY) {
+                galaxy_locations.push_back(std::make_pair(row, col));
+           }
+       }
+   }
+
+   std::vector<std::pair<std::pair<unsigned int, unsigned int>, std::pair<unsigned int, unsigned int>>> galaxy_pairs = {};
+   for (auto galaxy_1 : galaxy_locations) {
+       for (auto galaxy_2 : galaxy_locations) {
+           if ( // avoid adding duplicate pair entries by these checks
+               (galaxy_1 != galaxy_2) &&
+               (std::find(galaxy_pairs.begin(), galaxy_pairs.end(), std::make_pair(galaxy_1, galaxy_2)) == galaxy_pairs.end()) &&
+               (std::find(galaxy_pairs.begin(), galaxy_pairs.end(), std::make_pair(galaxy_2, galaxy_1)) == galaxy_pairs.end()))
+               galaxy_pairs.push_back(std::make_pair(galaxy_1, galaxy_2));
+       }
+   }
+
+   // calculate shortest lengths
+   std::vector<unsigned int> shortest_path_lengths = {};
+   for (auto pairs : galaxy_pairs)
+   {
+       shortest_path_lengths.push_back(
+           abs(std::get<0>(std::get<0>(pairs)) - std::get<0>(std::get<1>(pairs))) + abs(std::get<1>(std::get<0>(pairs)) - std::get<1>(std::get<1>(pairs))));
+   }
+   int sum_of_shortest_paths = std::accumulate(shortest_path_lengths.begin(), shortest_path_lengths.end(),
+                                               decltype(shortest_path_lengths)::value_type(0));
+
+   std::cout << "The sum of shortest paths is: " << sum_of_shortest_paths << '\n';
 }
