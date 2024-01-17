15ecdee2a8e054db093c581e52417af9d3109bdd
Ludwig Lohmer <ludwig-lohmer@web.de>
2024-01-17 15:02:51 +0100

    Add new functions for file utilities
    placed in parent directory such that all days' tasks can access it

---
+++ file_utils.h
@@ -0,0 +1,40 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+#include <iostream>
+#include <fstream>
+
+
+namespace FileUtils {
+
+    enum class InputFileStatus {
+        ok = 0,
+        no_input_given,
+        input_file_does_not_exist 
+    };
+
+    InputFileStatus get_file_status(int argc, char** argv)
+    {
+        /*
+        Checks the status of the given input file.
+        */
+
+        // check for given input file, read in file and check for file existance first
+        if (argc <= 1) {
+            std::cout << "No input file given.\n";
+            return InputFileStatus::no_input_given;
+        }
+        std::ifstream text_file(argv[1]);
+
+        if (!text_file.good()) {
+            std::cout << "Given text file \"" << argv[1] << "\" does not exist.\n";
+            return InputFileStatus::input_file_does_not_exist;
+        }
+        
+        return InputFileStatus::ok;
+    }
+
+};
+
+
+#endif
