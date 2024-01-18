fbba726015e9cccaed6bbad5365dd10112c7db8b
Ludwig Lohmer <ludwig-lohmer@web.de>
2024-01-18 17:28:36 +0100

    Start with problem 15
    reading in of data; function boilerplate for hash algorithm;
    CmakeLists.txt

---
+++ 15/main.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "../file_utils.h"
+
+
+const char CHAR_SEPARATOR = ',';
+
+
+unsigned int compute_hash_value(std::string& str)
+{
+    unsigned int current_value = 0;
+
+    for (char c : str) {
+        current_value += (c);
+        current_value *= 17;
+        current_value = current_value % 256;
+    }
+
+    return current_value;
+}
+
+
+int main(int argc, char** argv)
+{
+    if (FileUtils::get_file_status(argc, argv) != FileUtils::InputFileStatus::ok) {
+        return -1;
+    }
+    std::fstream text_file(argv[1]);
+
+    // read in line by line and add the patterns to a vector
+    // (there should only be one line in this puzzle's input)
+    std::vector<std::string> initialization_sequence = {};
+    for (std::string line; std::getline(text_file, line);) {
+
+        std::istringstream str_stream(line);
+        for (std::string val; std::getline(str_stream, val, CHAR_SEPARATOR);) {
+            initialization_sequence.push_back(val);
+        }
+    }
+
+    std::cout << "Check\n";
+}
