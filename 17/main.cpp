dd1787382e3b965e56a7db22133e62c73ae8ac26
Ludwig Lohmer <ludwig-lohmer@web.de>
2024-02-15 16:02:32 +0100

    Begin with day 17's problem
    reading in of file

---
+++ 17/main.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include "file_utils.h"
+
+int main(int argc, char **argv)
+{
+    if (FileUtils::get_file_status(argc, argv) == FileUtils::InputFileStatus::ok) {
+        return -1;
+    }
+    std::fstream text_file(argv[1]);
+
+    // read in file
+    std::vector<std::string> city = {};
+    for (std::string line; std::getline(text_file, line);) {
+
+        city.push_back(line);
+
+    }
+
+    // ...
+    
+}
